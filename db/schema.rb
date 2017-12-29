1132fdf98085ee18ad0198f040a3b4bc79ce922c
Brad Carlson <b2carlso@yahoo.com>
2017-12-29 20:57:10 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,11 @@
 
 ActiveRecord::Schema.define(version: 20171229203912) do
 
+  create_table "tweets", force: :cascade do |t|
+    t.string "content"
+    t.integer "user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
